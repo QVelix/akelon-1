8e29fc6c35b44a362657afd2d04e8e911bde9d2c
MaximBogachev <m.bogachev.prog@solo-it.ru>
2023-12-13 08:00:41 +0400

    Написал класс со свойствами клиента

---
+++ Classes/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace akelon_1.Classes;
+
+public class Client
+{
+    public int Code { get; set; }
+    public string CompanyName { get; set; }
+    public string ContactPerson { get; set; }
+    public string Address { get; set; }
+}
