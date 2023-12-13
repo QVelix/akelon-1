eecc833c33860c8b4951b669d58fcd0db2b77682
MaximBogachev <m.bogachev.prog@solo-it.ru>
2023-12-13 07:58:16 +0400

    Написал свойства товара

--- Classes/Product.cs
+++ Classes/Product.cs
@@ -2,5 +2,8 @@
 
 public class Product
 {
-    
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public string MeasureUnit { get; set; }
+    public decimal Price { get; set; }
 }
