499099a15ca07af3c5caad084ce442949f5d1df8
MaximBogachev <m.bogachev.prog@solo-it.ru>
2023-12-13 08:13:54 +0400

    Написал обработку команды help

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+if ((args.Contains("help") || args.Contains("h"))&&(!args.Contains("command")||!args.Contains("c")))
+{
+    Console.WriteLine("Список команд приложения:\n" +
+                      "* help/h - Вывести список команд\n" +
+                      "* filepath/f - Ввести путь до файла. Пример: filepath=/home/user/download/myExcel.xlsx\n" +
+                      "* command/c - Выполнить команду. Пример: command=GetGoldenClient\n" +
+                      "* command-help/ch - Получить список доступных команд");
+    return;
+}
+
+if (args.Contains("filepath") || args.Contains("f"))
+{
+    
+}
