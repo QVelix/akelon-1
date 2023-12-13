781142c9252cac557eb7bc42bc0c60128d502ebd
MaximBogachev <m.bogachev.prog@solo-it.ru>
2023-12-13 08:04:23 +0400

    Написал свойства для заявки

--- Classes/Request.cs
+++ Classes/Request.cs
@@ -2,5 +2,10 @@
 
 public class Request
 {
-    
+    public int Code { get; set; }
+    public int ProductCode { get; set; }
+    public int ClientCode { get; set; }
+    public int Number { get; set; }
+    public int Count { get; set; }
+    public DateOnly PlacementDate { get; set; }
 }
