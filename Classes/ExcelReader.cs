774ac0d844081886a1e23312945ec85544639fe3
MaximBogachev <m.bogachev.prog@solo-it.ru>
2023-12-15 07:09:08 +0400

    Создал класс для работы с экселем

---
+++ Classes/ExcelReader.cs
@@ -0,0 +1,15 @@
+﻿using ClosedXML.Excel;
+
+namespace akelon_1.Classes;
+
+public class ExcelReader
+{
+    private XLWorkbook excelFile;
+
+    public ExcelReader(string filepath)
+    {
+        excelFile = new XLWorkbook(filepath);
+    }
+
+    
+}
