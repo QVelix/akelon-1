5b49eb9456972422c29796b1d64de26167f9ab9c
MaximBogachev <m.bogachev.prog@solo-it.ru>
2023-12-15 07:18:05 +0400

    Написал метод для чтения из эксель товаров

--- Classes/ExcelReader.cs
+++ Classes/ExcelReader.cs
@@ -11,5 +11,20 @@ public class ExcelReader
         excelFile = new XLWorkbook(filepath);
     }
 
-    
+    public List<Product> GetProducts()
+    {
+        List<Product> products = new List<Product>();
+        IXLWorksheet worksheet = excelFile.Worksheet(1);
+        int worksheetRows = worksheet.Rows().Count();
+        for (int i = 1; i < worksheetRows; i++)
+        {
+            Product product = new Product();
+            product.Code = Convert.ToInt32(worksheet.Cell(i, 1).Value);
+            product.Name = Convert.ToString(worksheet.Cell(i, 2).Value);
+            product.MeasureUnit = Convert.ToString(worksheet.Cell(i, 3).Value);
+            product.Price = Convert.ToDecimal(worksheet.Cell(i, 4).Value);
+            products.Add(product);
+        }
+        return products;
+    }
 }
